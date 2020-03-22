ab155fb322f1d3b5250d785289487c6d9219cf97
sterrestelsel <toscha.nv@gmail.com>
2020-03-22 18:16:11 +0700

    Added form to upload mp3

--- task.py
+++ task.py
@@ -1,17 +1,18 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['mp3'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
 @app.route('/home')
 def hello():
     return render_template('index.html')
-
-
-@app.route('/about')
-def about():
-    return "it's useless page"
 
 
 if __name__ == '__main__':
